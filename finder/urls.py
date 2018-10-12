7a7cffcd803c9415c8f411106266b8fc5dd6f8d4
Jake S <skrobot1@msu.edu>
2018-10-12 13:50:50 -0500

    added delete and edit for finder posts

--- finder/urls.py
+++ finder/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', FinderPageView.as_view(), name='home'),
     path('<int:pk>/', FinderDetailView.as_view(), name='finder_detail'),
     path('new/', FinderNewView.as_view(), name='finder_add'),
+    path('<int:pk>/edit/', FinderEditView.as_view(), name='finder_edit'),
+    path('<int:pk>/delete/', FinderDeleteView.as_view(), name='finder_delete'),
 ]
--- finder/views.py
+++ finder/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -17,4 +18,14 @@ class FinderNewView(CreateView):
     model = Post
     template_name = 'finder_new.html'
     fields = ['title', 'author', 'body']
+
+class FinderEditView(UpdateView):
+    model = Post
+    template_name = 'finder_edit.html'
+    fields = ['title', 'body']
+
+class FinderDeleteView(DeleteView):
+    model = Post
+    template_name = 'finder_delete.html'
+    success_url = reverse_lazy('finder:home')
 
